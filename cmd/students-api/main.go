51627f9efec84ee7c786a796104f1a7390927c8d
Govind Kulkarni <govindakulkarni007@gmail.com>
2024-12-30 18:25:01 +0530

    add request validation

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/govinda2000/students-api/internal/config"
+	"github.com/govinda2000/students-api/internal/http/handlers/student"
 )
 
 func main() {
@@ -22,9 +23,7 @@ func main() {
 	// setup router
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
 	//setup server
 	server := http.Server{
@@ -32,7 +31,7 @@ func main() {
 		Handler: router,
 	}
 
-	slog.Info("Server Started on port: %s", slog.String("address", cfg.Addr))
+	slog.Info("Server Started on port:", slog.String("address", cfg.Addr))
 
 	done := make(chan os.Signal, 1)
 
